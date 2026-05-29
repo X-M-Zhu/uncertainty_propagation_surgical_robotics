02ea7c29e4e3ee7f8578fda225721ea4cca036c3
Aiza Maksutova <71812108+aizamaksutova@users.noreply.github.com>
2026-05-29 10:07:13 -0400

    Add inverse of rotation matrix and frame

--- src/uncertainty_networks/spatial_math.py
+++ src/uncertainty_networks/spatial_math.py
@@ -38,6 +38,7 @@ class Point:
 
 
 # TBD: rotations along multiple axis through the ZYX euler angle convention
+# Add inverse of Rotation matrix
 class Rot:
     def __init__(self, axis: str = None, angle: float = None, matrix: np.ndarray = None):
         if matrix is not None:
@@ -73,7 +74,7 @@ class Rot:
             return Rot(matrix=self._matrix @ other.matrix)
         else:
             raise TypeError("Unsupported multiplication")
-
+# Add inverse of frame 
 class Frame:
     def __init__(self, R: Rot, p: Point):
         self.R = R
